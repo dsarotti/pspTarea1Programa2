8c7be4077365c273b512cebd1a76e5746c3bfebd
dsarotti <sr.sarotti@gmail.com>
2023-10-03 12:52:22 +0200

    Añadido control de errores, Añadidas las clases al directorio '\bin'

--- src/App.java
+++ src/App.java
@@ -1,18 +1,19 @@
+import java.io.IOException;
+
 public class App {
     /** 
-     * Esta aplicación 
+     * Esta aplicación ejecuta un proceso externo.
      * @param args
      */
     public static void main(String[] args) {
-        // Especificar la ruta al archivo JAR
-        //final String rutaJar = "C:\\Users\\desarotti\\Documents\\Programación de procesos e servicios\\Tarea 1\\Tarea 1\\Tarea1.jar";
-        final String rutaJar = ".\\bin";
+        // Especificar la ruta a la carpeta con las clases
+        final String rutaClases = ".\\bin";
         try {
             // Crea un objeto ProcessBuilder
             //ProcessBuilder builder = new ProcessBuilder("java", "-jar", rutaJar);
-            ProcessBuilder builder = new ProcessBuilder("java","-cp", rutaJar,"Tarea1");
+            ProcessBuilder builder = new ProcessBuilder("java","-cp", rutaClases,"Tarea1");
 
-            //Redireccionar las entradas y salidas del proceso hijo a las mismas del proceso actual.
+            // Redireccionar las entradas y salidas del proceso hijo a las del proceso actual.
             builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
             builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
             builder.redirectErrorStream(true);
@@ -20,16 +21,27 @@ public class App {
             // Inicia el proceso
             Process proceso = builder.start();
 
-            // Espera a que el proceso termine
+            // Espera a que el proceso termine guardando el código de retorno
             int retornoProceso = proceso.waitFor();
 
-            // Puedes verificar el valor de retorno si lo deseas
-            System.out.println("\nEl proceso ha terminado con código de retorno: " + retornoProceso);
+            // Verifica el valor de retorno del proceso e informa al usuario
+            if (retornoProceso == 0) {
+                System.out.println("\nEl proceso ha terminado exitosamente.");
+            } else {
+                System.err.println("\nEl proceso ha terminado con código de retorno: " + retornoProceso);
+            }
 
+        } catch (IOException e) {
+            // Manejar excepciones de E/S (por ejemplo, clase principal no encontrada)
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // Manejar interrupciones del proceso de espera
+            e.printStackTrace();
         } catch (Exception e) {
+            // Manejar otras excepciones
             e.printStackTrace();
         }
 
         System.out.println("\nSaliendo del programa.");
-    }
+        }
 }
